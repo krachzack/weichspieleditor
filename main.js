f28d2713b7b4f274489ec98bb7904a1224310504
krachzack <hello@phstadler.com>
2019-10-04 21:49:59 +0200

    feat: add window
    Load a browser window on start up and launch the server when it is open. When the server is ready, show a message in the window.

---
+++ main.js
@@ -0,0 +1,40 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const launchRuntime = require('./server/runtime.js')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    console.log('launching runtime...')
+    launchRuntime().then(
+      ({ port }) => {
+        console.log(`runtime is up and running on port ${port}`)
+        mainWindow.webContents.send('fernspielapparatReady', port)
+      },
+      err => mainWindow.webContents.send('fernspielapparatError', err)
+    )
+  })
+
+  mainWindow.on('closed', function () {
+    // Free the window
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  app.quit()
+})
---
+++ preload.js
@@ -0,0 +1,35 @@
+
+const { ipcRenderer } = require('electron')
+
+Promise.all([waitForServerStart(), waitForDomReady()])
+  .then(
+    ([port]) => {
+      document.getElementById('server-message')
+        .textContent = `Server running on port ${port}.`
+    }
+  )
+
+function waitForServerStart () {
+  return new Promise((resolve, reject) => {
+    ipcRenderer.once(
+      'fernspielapparatReady',
+      (_evt, port) => {
+        resolve(port)
+      }
+    )
+    ipcRenderer.once(
+      'fernspielapparatError',
+      (_evt, err) => {
+        reject(err)
+      }
+    )
+  })
+}
+
+function waitForDomReady () {
+  return new Promise((resolve, reject) => {
+    window.addEventListener('load', () => {
+      resolve()
+    })
+  })
+}
