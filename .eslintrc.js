031a18e6ead776c632fc4a149ac0724b4a5856fe
krachzack <hello@phstadler.com>
2019-10-04 16:17:02 +0200

    chore: add .eslintrc.js
    Add a config file for ESLint, which can be used for linting and fixing style problems.

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es6": true
+  },
+  "extends": [
+      "plugin:vue/recommended",
+      "standard"
+  ],
+  "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly",
+      "test": "readonly",
+      "expect": "readonly",
+      "describe": "readonly"
+  },
+  "parserOptions": {
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "vue"
+  ],
+  "rules": {
+      "vue/html-self-closing": "off"
+  }
+}
